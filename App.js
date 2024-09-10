58904157f387e28c9e2e7160fc29995e9a419d34
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2024-09-10 23:46:49 +0530

    creating components

--- App.js
+++ App.js
@@ -1,6 +1,59 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const heading=React.createElement("h1",{id:"hello"},"hello using react")
-const root=ReactDOM.createRoot(document.getElementById('root'))
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"h1-data"},"hello nested h1"),React.createElement("h2",{id:"h2-data"},"hello nested h2")]))
-root.render(parent)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ *  -Logo
+ *  -Nav items
+ * Body
+ *  -Restorent Container
+ *   -Search Bar
+ * Footer
+ *   -copyright
+ * -Links
+ * -Address
+ * -Contact
+ * 
+ */
+
+const Header=()=>
+{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                 <img  className="logo" src="https://img.freepik.com/free-vector/hand-drawn-cs-food-logo_23-2149385086.jpg"></img>
+            </div>
+            <div className="Nav-items  ">
+                <ul >
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+const Body=()=>
+{
+    return(<div className="body">
+        <div className="search">
+        
+        </div>
+        <div className="re-cards"></div>
+    </div>)
+}
+
+const AppLayout=()=>(
+    <div className="app">
+    <Header/>
+        </div>
+)
+
+const root= ReactDOM.createRoot(document.getElementById("root"))
+let data=100;
+root.render(
+<AppLayout/>
+);
